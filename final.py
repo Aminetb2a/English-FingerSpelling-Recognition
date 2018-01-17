62378be7201ad1e7494d655d9830a7a349db93a3
amine <aminetbo@gmail.com>
2018-01-17 15:03:40 +0300

    first

---
+++ final.py
@@ -0,0 +1,353 @@
+# -*- coding: utf-8 -*-
+
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# Import libraries
+import os,cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import json
+import glob
+from sklearn.utils import shuffle
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from keras import backend as K
+K.set_image_dim_ordering('th')
+from PIL import Image
+
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.optimizers import SGD,RMSprop,adam
+
+
+#%%
+
+PATH = os.getcwd()
+# Define data path
+data_path = PATH + '/data'
+data_dir_list = os.listdir(data_path)
+
+img_rows=128
+img_cols=128
+num_channel=1
+num_epoch=5
+
+# Define the number of classes
+num_classes =24
+
+names = ['A','B','C','D','E','F','G','H','I','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y']
+
+global get_output
+img_data_list=[]
+
+#%%
+for dataset in data_dir_list:
+	img_list=os.listdir(data_path+'/'+ dataset)
+	print ('Loaded the images of dataset-'+'{}\n'.format(dataset))
+	for img in img_list:
+		input_img=cv2.imread(data_path + '/'+ dataset + '/'+ img )
+		input_img=cv2.cvtColor(input_img, cv2.COLOR_BGR2GRAY)
+		input_img_resize=cv2.resize(input_img,(128,128))
+		img_data_list.append(input_img_resize)
+
+img_data = np.array(img_data_list)
+print(img_data.shape)
+img_data = img_data.astype('float32')
+print(img_data.shape)
+img_data /= 255
+print (img_data.shape)
+
+
+images_paths = glob.glob( os.path.join("data",'*','*'))
+img_data_list, labels = [], []
+    # print("img path",images_paths)
+for img in images_paths:
+    print (img)
+    labels.append(img.split('\\')[-2])
+
+print ("before ",np.unique(labels))
+labels = LabelEncoder().fit_transform(labels)
+
+
+
+
+if num_channel==1:
+	if K.image_dim_ordering()=='th':   #theano (210,128,128)
+		img_data= np.expand_dims(img_data, axis=1)    #(210,1,128,128)
+		print (img_data.shape)
+	else:      #tensorflow  
+		img_data= np.expand_dims(img_data, axis=4)    #(210,128,128,1)
+		print (img_data.shape)
+		
+else:       #if not using channel 1, instead using RGB
+	if K.image_dim_ordering()=='th':
+		img_data=np.rollaxis(img_data,3,1) #(210,128,128,3) or (210,3,128,128)
+		print (img_data.shape)
+        
+
+
+#%%
+    
+#num_of_samples = img_data.shape[0]
+#labels = np.ones((num_of_samples,),dtype='int64')
+
+#labels[0:70]=0
+#labels[70:140]=1
+#labels[140:]=2
+
+	  	  
+# convert class labels to on-hot encoding
+Y = np_utils.to_categorical(labels, num_classes)
+
+#Shuffle the dataset
+x,y = shuffle(img_data,Y, random_state=2)
+# Split the dataset
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=2)
+
+input_shape=img_data[0].shape
+
+#%%					
+model = Sequential()
+
+model.add(Conv2D(32, (3,3),input_shape=input_shape,padding="same"))
+model.add(Activation('relu'))
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
+
+model.add(Conv2D(64, (3, 3)))#6
+model.add(Activation('relu'))
+model.add(Conv2D(128, (3, 3)))#6
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
+
+model.add(Flatten())
+model.add(Dense(128))
+model.add(Activation('relu'))
+model.add(Dropout(0.2))
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.2))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
+
+#%%
+
+#sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+#model.compile(loss='categorical_crossentropy', optimizer=sgd,metrics=["accuracy"])
+model.compile(loss='categorical_crossentropy', optimizer='rmsprop',metrics=["accuracy"])
+#layer = model.layers[:-1]
+#get_output = K.function([model.layers[0].input, K.learning_phase()], [model.layers[:-1].output,])
+hist = model.fit(X_train, y_train, batch_size=25, epochs=num_epoch, verbose=1, validation_data=(X_test, y_test))
+
+np.shape(model.layers)
+model.summary()
+model.get_config()
+model.layers[0].get_config()
+model.layers[0].input_shape			
+model.layers[0].output_shape			
+model.layers[0].get_weights()
+np.shape(model.layers[0].get_weights()[0])
+model.layers[0].trainable
+
+#%%
+
+# visualizing losses and accuracy
+train_loss=hist.history['loss']
+val_loss=hist.history['val_loss']
+train_acc=hist.history['acc']
+val_acc=hist.history['val_acc']
+xc=range(num_epoch)
+
+plt.figure(1,figsize=(7,5))
+plt.plot(xc,train_loss)
+plt.plot(xc,val_loss)
+plt.xlabel('num of Epochs')
+plt.ylabel('loss')
+plt.title('train_loss vs val_loss')
+plt.grid(True)
+plt.legend(['train','val'])
+#print plt.style.available # use bmh, classic,ggplot for big pictures
+plt.style.use(['classic'])
+
+plt.figure(2,figsize=(7,5))
+plt.plot(xc,train_acc)
+plt.plot(xc,val_acc)
+plt.xlabel('num of Epochs')
+plt.ylabel('accuracy')
+plt.title('train_acc vs val_acc')
+plt.grid(True)
+plt.legend(['train','val'],loc=4)
+#print plt.style.available # use bmh, classic,ggplot for big pictures
+plt.style.use(['classic'])
+
+#%%
+
+# Evaluating the model
+
+score = model.evaluate(X_test, y_test, verbose=0)
+print('Test Loss:', score[0])
+print('Test accuracy:', score[1])
+
+test_image = X_test[0:1]
+print ("test_image.shape: ",test_image.shape)
+
+print("predict: ",model.predict(test_image))
+print("predict_shape: ",model.predict(test_image).shape)
+print("predicted class: ",model.predict_classes(test_image))
+x=y_test[0:1]
+print("y test: ",x)
+print("y test: ",x.shape)        
+#confusion_matrix(test_image,x)
+# -*- coding: utf-8 -*-
+
+# Testing a new image
+test_image = cv2.imread('data/h/006.jpg')
+test_image=cv2.cvtColor(test_image, cv2.COLOR_BGR2GRAY)
+test_image=cv2.resize(test_image,(128,128))
+test_image = np.array(test_image)
+test_image = test_image.astype('float32')
+test_image /= 255
+print (test_image.shape)
+   
+if num_channel==1:
+	if K.image_dim_ordering()=='th':
+		test_image= np.expand_dims(test_image, axis=0)
+		test_image= np.expand_dims(test_image, axis=0)
+		print (test_image.shape)
+	else:
+		test_image= np.expand_dims(test_image, axis=3) 
+		test_image= np.expand_dims(test_image, axis=0)
+		print (test_image.shape)
+		
+else:
+	if K.image_dim_ordering()=='th':
+		test_image=np.rollaxis(test_image,2,0)
+		test_image= np.expand_dims(test_image, axis=0)
+		print (test_image.shape)
+	else:
+		test_image= np.expand_dims(test_image, axis=0)
+		print (test_image.shape)
+		
+# Predicting the test image
+print((model.predict(test_image)))
+print(model.predict_classes(test_image))
+
+#%%
+
+# Visualizing the intermediate layer
+
+#
+def get_featuremaps(model, layer_idx, X_batch):
+	get_activations = K.function([model.layers[0].input, K.learning_phase()],[model.layers[layer_idx].output,])
+	activations = get_activations([X_batch,0])
+	return activations
+
+layer_num=6
+filter_num=0
+
+activations = get_featuremaps(model, int(layer_num),test_image)
+
+print (np.shape(activations))
+feature_maps = activations[0][0]      
+print (np.shape(feature_maps))
+
+if K.image_dim_ordering()=='th':
+	feature_maps=np.rollaxis((np.rollaxis(feature_maps,2,0)),2,0)
+print (feature_maps.shape)
+
+fig=plt.figure(figsize=(16,16))
+plt.imshow(feature_maps[:,:,filter_num],cmap='gray')
+plt.savefig("featuremaps-layer-{}".format(layer_num) + "-filternum-{}".format(filter_num)+'.jpg')
+
+num_of_featuremaps=feature_maps.shape[2]
+fig=plt.figure(figsize=(16,16))	
+plt.title("featuremaps-layer-{}".format(layer_num))
+subplot_num=int(np.ceil(np.sqrt(num_of_featuremaps)))
+for i in range(int(num_of_featuremaps)):
+	ax = fig.add_subplot(subplot_num, subplot_num, i+1)
+	#ax.imshow(output_image[0,:,:,i],interpolation='nearest' ) #to see the first filter
+	ax.imshow(feature_maps[:,:,i],cmap='gray')
+	plt.xticks([])
+	plt.yticks([])
+	plt.tight_layout()
+plt.show()
+fig.savefig("featuremaps-layer-{}".format(layer_num) + '.jpg')
+
+#%%
+# Printing the confusion matrix
+from sklearn.metrics import classification_report,confusion_matrix
+import itertools
+
+Y_pred = model.predict(X_test)
+print(Y_pred)
+y_pred = np.argmax(Y_pred, axis=1)
+print(y_pred)
+#y_pred = model.predict_classes(X_test)
+#print(y_pred)
+target_names = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y']
+					
+print(classification_report(np.argmax(y_test,axis=1), y_pred,target_names=target_names))
+
+print(confusion_matrix(np.argmax(y_test,axis=1), y_pred))
+
+
+# Plotting the confusion matrix
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+# Compute confusion matrix
+cnf_matrix = (confusion_matrix(np.argmax(y_test,axis=1), y_pred))
+
+np.set_printoptions(precision=2)
+
+plt.figure()
+
+# Plot non-normalized confusion matrix
+plot_confusion_matrix(cnf_matrix, classes=target_names,
+                      title='Confusion matrix')
+#plt.figure()
+# Plot normalized confusion matrix
+#plot_confusion_matrix(cnf_matrix, classes=target_names, normalize=True,
+#                      title='Normalized confusion matrix')
+#plt.figure()
+plt.show()
